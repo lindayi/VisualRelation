a85fa3df8cfa870bc06c0b146730fe4d980eb777
nonodevil <xiyou.nonodevil@gmail.com>
2013-11-04 22:35:19 +0800

    修复一些json封装bug

--- demo/getLinkInfo.php
+++ demo/getLinkInfo.php
@@ -1,6 +1,26 @@
 <?php 
-    include_once "conn.php";
+    /*
+     * @description: encoding mysql result set to json
+     * @format: {
+            'ne': xxx, 
+            'type': xxx, 
+            'sources':[
+                {
+                    'text': xxx,
+                    'realtime': xxx,
+                    'pubtime': xxx,
+                },
+                {
+                    'text': xxx,
+                    'realtime': xxx,
+                    'pubtime': xxx,
+                },
+            ]
+            }
+     *
+     */
 
+    include_once "conn.php";
     $source = $_GET['source'];
     $target = $_GET['target'];
 
@@ -15,19 +35,12 @@
         'realtime' => $row['realtime'],
         'pubtime' => $row['pubtime']
     );
-    // array_push($source, 'text' => $row['text']);
-    // array_push($source, 'realtime' => $row['realtime']);
-    // array_push($source, 'pubtime' => $row['pubtime']);
 
     $sources = array();
     array_push($sources, $source);
 
-    $link = array(
-        "type" => $row['type'],
-        "ne" => $row['ne']
-    );
-    // array_push($link, "type" => $row['type']);
-    // array_push($link, "ne" => $row['ne']);
+    $type = $row['type'];
+    $ne   = $row['ne'];
 
     while ($row = mysql_fetch_array($result)) {
         $source = array(
@@ -35,13 +48,14 @@
         'realtime' => $row['realtime'],
         'pubtime' => $row['pubtime']
         );
-        // array_push($source, 'text' => $row['text']);
-        // array_push($source, 'realtime' => $row['realtime']);
-        // array_push($source, 'pubtime' => $row['pubtime']);
-
         array_push($sources, $source);
     }
-    array_push($link, $sources);
-    
-   echo json_encode($link);
+
+    $link = array(
+        'type'=>$type,
+        'ne'=>$ne,
+        'sources'=>$sources
+        );
+
+    echo json_encode($link);
  ?>
