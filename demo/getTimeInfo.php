67d087c39864577af9587ad80992536f92f0a252
nonodevil <xiyou.nonodevil@gmail.com>
2013-11-23 15:04:09 +0800

    封装json

--- demo/getTimeInfo.php
+++ demo/getTimeInfo.php
@@ -1,33 +1,6 @@
 <?php
-
-	function proctime($time, $t)
-	{
-		if (($t == 1)&&(strlen($time) > 15)) $time = substr($time, 15, 12);
-		else $time = substr($time, 0, 12);
-		
-		return substr($time, 0, 4).",".substr($time, 4, 2).",".substr($time, 6, 2);
-	}
-
-	include_once "conn.php";
-	$titleheadline = $_GET['keyword'];
-	$id = $_GET['id'];
-	$titleheadline = $titleheadline." 的时间轴";
-	$count = 0;
-
-	$sqlquery = "SELECT DISTINCT destid, ne, realtime, type FROM relation WHERE sourceid = '".$id. "' and type not in ( \"CeMODIFIERS\",\"CeXMODIFIER_OF\")";
-	$result = mysql_query($sqlquery);
-	$row = mysql_fetch_array($result);
-	while($row = mysql_fetch_array($result))
-	{
-		$startDate[$count] = proctime($row["realtime"],0);
-		$endDate[$count] = proctime($row["realtime"],1);
-		$headline[$count] = $row["ne"];
-		$text[$count] = "<p>来源：</p>";
-		//$sqlquery = "SELECT DISTINCT destid, type, text FROM relation, doc WHERE destid = ".$row["destid"]." and sourceid = ".$id." and type = ".
-		$count++;
-	}
-?>
-
+	/*
+	
 	{
 		"timeline":
 		{
@@ -48,3 +21,61 @@
 			"era": []
 		}
 	}
+	*/
+
+	function proctime($time, $t)
+	{
+		if (($t == 1)&&(strlen($time) > 15)) $time = substr($time, 15, 12);
+		else $time = substr($time, 0, 12);
+		
+		return substr($time, 0, 4).",".substr($time, 4, 2).",".substr($time, 6, 2);
+	}
+
+	include_once "conn.php";
+	$titleheadline = $_GET['keyword'];
+	$id = $_GET['id'];
+	$titleheadline = $titleheadline." 的时间轴";
+	$count = 0;
+
+	$sqlquery = "SELECT DISTINCT destid, ne, realtime, type FROM relation WHERE sourceid = '".$id. "' and type not in ( \"CeMODIFIERS\",\"CeXMODIFIER_OF\")";
+	$result = mysql_query($sqlquery);
+	$row = mysql_fetch_array($result);
+
+	$dates = array();
+	while($row = mysql_fetch_array($result))
+	{
+		$startDate[$count] = proctime($row["realtime"],0);
+		$endDate[$count] = proctime($row["realtime"],1);
+		$headline[$count] = $row["ne"];
+		$text[$count] = "<p>来源：</p>";
+		//$sqlquery = "SELECT DISTINCT destid, type, text FROM relation, doc WHERE destid = ".$row["destid"]." and sourceid = ".$id." and type = ".
+		
+		$asset = array();
+		$date = array(
+			"startDate"=>$startDate[$count],
+			"endDate"=>$endDate[$count],
+			"headline"=>$headline[$count],
+			"text"=>$text[$count],
+			"tag"=>" ",
+			"asset"=>$asset
+			);
+		array_push($dates, $date);
+		$count++;
+	}
+
+	$asset = array();
+	$era = array();
+	$timeline = array(
+		"headline"=>$titleheadline,
+		"type"=>"default",
+		"text"=>"<p>点击右侧箭头，进入时间轴</p>",
+		"asset"=>$asset,
+		"date"=>$dates,
+		"era"=>$era
+		);
+	$res = array(
+		"timeline"=>$timeline
+		);
+
+	echo json_encode($res);
+?>
