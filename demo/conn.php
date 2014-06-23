e0abf4465a62ed78e9f4c010ae81bf949ecd302a
Lin Dayi <heylindayi@gmail.com>
2014-06-23 16:06:24 +0800

    Update conn.php

--- demo/conn.php
+++ demo/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysql_connect("localhost:3306","root","woshilindayi");
-	mysql_select_db("visualv3", $conn);
+	$conn = mysql_connect("localhost:3306","数据库用户名","数据库密码");
+	mysql_select_db("库名", $conn);
 	mysql_query("set names utf8");
 ?>
